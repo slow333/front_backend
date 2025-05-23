9ecc9a5d94cba696eb86ed81b5954a30d5490e74
kim kwan yong <slow33@gmail.com>
2025-05-24 08:30:31 +0900

    db first step done

--- src/main/java/spring/frontend_backend/urlcontroller/Database.java
+++ src/main/java/spring/frontend_backend/urlcontroller/Database.java
@@ -7,50 +7,62 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/database")
 public class Database {
-    // =====================================================================================
-  // ############### 1장: w3-Mysql #################
-  // =====================================================================================
+  // ==============================================
+  // ############## 1장: w3-Mysql #################
+  // ==============================================
   @GetMapping("/01-w3/00-mysql-basic")
   public String w3MysqlBasic() {
     return "database/01-w3/00-mysql-basic";
   }
 
-  @GetMapping("/01-w3/01-create")
+  @GetMapping("/01-w3/01-db-table-handle")
   public String w3Create() {
-    return "database/01-w3/01-create";
+    return "database/01-w3/01-db-table-handle";
   }
 
-  @GetMapping("/01-w3/02-select")
+  @GetMapping("/01-w3/02-constraints")
+  public String w3constraints() {
+    return "database/01-w3/02-constraints";
+  }
+
+  @GetMapping("/01-w3/03-select")
   public String w3select() {
-    return "database/01-w3/02-select";
+    return "database/01-w3/03-select";
   }
-  @GetMapping("/01-w3/03-insert-update-delete")
+
+  @GetMapping("/01-w3/04-insert-update-delete")
   public String w3InsertUpdateDelete() {
-    return "database/01-w3/03-insert-update-delete";
+    return "database/01-w3/04-insert-update-delete";
   }
-  @GetMapping("/01-w3/04-aggregate")
-  public String w3Aggregate() {
-    return "database/01-w3/04-aggregate";
-  }
-  @GetMapping("/01-w3/05-like-wildcard")
+  
+  @GetMapping("/01-w3/05-select-search-like-in-between")
   public String w3LikeWildcard() {
-    return "database/01-w3/05-like-wildcard";
+    return "database/01-w3/05-select-search-like-in-between";
   }
-  @GetMapping("/01-w3/06-in-between-alias")
-  public String w3InBetween() {
-    return "database/01-w3/06-in-between-alias";
+
+  @GetMapping("/01-w3/06-aggregate")
+  public String w3Aggregate() {
+    return "database/01-w3/06-aggregate";
   }
+
   @GetMapping("/01-w3/07-join")
   public String w3Join() {
     return "database/01-w3/07-join";
   }
+
   @GetMapping("/01-w3/08-group-by-having")
   public String w3GroupByHaving() {
     return "database/01-w3/08-group-by-having";
   }
+
   @GetMapping("/01-w3/09-exists-any-all")
   public String w3ExistsAnyAll() {
     return "database/01-w3/09-exists-any-all";
+  }
+
+  @GetMapping("/01-w3/10-copy-table-insert-select")
+  public String w3CopyTable() {
+    return "database/01-w3/10-copy-table-insert-select";
   }
   
   // =====================================================================================
